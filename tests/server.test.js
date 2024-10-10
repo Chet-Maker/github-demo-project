7ff8620ee1a1bb108383eba6e6d061c93a8eba27
Chet Backiewicz <chetmaker1@gmail.com>
2024-10-10 19:11:58 -0400

    Add Jest tests and Supertest for server

---
+++ tests/server.test.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+describe('GET /', () => {
+    it('should return 200 OK', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+    });
+
+    it('should contain the correct title', async () => {
+        const response = await request(app).get('/');
+        expect(response.text).toContain('Welcome to GitHub Demo Project');
+    });
+});
